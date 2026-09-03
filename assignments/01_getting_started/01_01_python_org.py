d54514fc2330e3f7040d1b56bbdef57b20619f93
Timothy-Vaculik <timothy.vaculik001@albrigh.edu>
2026-09-03 17:31:26 -0400

    1st

---
+++ assignments/01_getting_started/01_01_python_org.py
@@ -0,0 +1,12 @@
+'''
+
+Look for python.org and write about stuff in the page
+
+'''
+
+'''
+
+This is going to be a good assignment to get started 
+
+'''
+
---
+++ assignments/02_variables_and_data_types/famous_quote.py
@@ -0,0 +1,5 @@
+author = "Joshua"
+
+phrase  = "Cashin - Cash Out - Cash Out"
+
+print (author + " said these things: " + phrase)
