ef2243e0ffc2173dc2b5869c1e38a7db17ab50cd
Timothy-Vaculik <timothy.vaculik001@albrigh.edu>
2026-09-07 09:24:59 -0400

    3rd

--- assignments/01_getting_started/01_01_python_org.py
+++ assignments/01_getting_started/01_01_python_org.py
@@ -10,3 +10,10 @@ This is going to be a good assignment to get started
 
 '''
 
+>>> print("Hello Python interpreter!")
+Hello Python interpreter!
+
+print("Hello Python world!")
+
+
+
--- assignments/02_variables_and_data_types/famous_quote.py
+++ assignments/02_variables_and_data_types/famous_quote.py
@@ -4,8 +4,5 @@ phrase  = "Cashin - Cash Out - Cash Out"
 
 print (author + " said these things: " + phrase)
 
->>> print("Hello Python interpreter!")
-Hello Python interpreter!
 
-print("Hello Python world!")
 
