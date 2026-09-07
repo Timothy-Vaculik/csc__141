a9ba3d43ddaa4129c45b9551422e947502e5f269
Timothy-Vaculik <timothy.vaculik001@albrigh.edu>
2026-09-07 09:36:18 -0400

    p14

--- assignments/02_variables_and_data_types/famous_quote.py
+++ assignments/02_variables_and_data_types/famous_quote.py
@@ -4,5 +4,10 @@ phrase  = "Cashin - Cash Out - Cash Out"
 
 print (author + " said these things: " + phrase)
 
+message = "Hello Python world!"
 
+print(message)
+
+name = "Timmy Vaculik"
+print(name.title())
 
