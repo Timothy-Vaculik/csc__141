0ecc01c83a55181ec7f193ef85edee3eee314fdc
Timothy-Vaculik <timothy.vaculik001@albrigh.edu>
2026-09-08 13:54:12 -0400

    13th

---
+++ assignments/02_variables_and_data_types/02_12_zen.py
@@ -0,0 +1,11 @@
+'''
+The Zen of Python, by Tim Peters
+
+Beautiful is better than ugly.
+
+'''
+'''
+This phython is a beautiful language that helps alot of coders find out many solutions. 
+To figure out how to find the solution, you have to find out how to use the language
+
+'''
