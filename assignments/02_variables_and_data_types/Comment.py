715dc90d3b89e974b1c4d370ac910bbf376eafe6
Timothy-Vaculik <timothy.vaculik001@albrigh.edu>
2026-09-08 09:41:40 -0400

    10th

---
+++ assignments/02_variables_and_data_types/Comment.py
@@ -0,0 +1,9 @@
+# Say hello to everyone.
+print("I love Python!")
+
+'''
+I like python because it is something new that I am learning and I am excited to learn more about it. I am looking forward to learning more about Python programming and improving my coding skills through this course.
+'''
+
+'''
+I think Java is cool because I have never learned it before and I am excited to learn more about it. I am looking forward to learning more about Java programming and improving my coding skills through this course.'''
