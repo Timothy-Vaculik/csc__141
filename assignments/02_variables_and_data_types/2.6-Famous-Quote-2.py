130e0a972337102798dbb6167eeca2f4326403f6
Timothy-Vaculik <timothy.vaculik001@albrigh.edu>
2026-09-08 09:38:50 -0400

    7th

--- assignments/02_variables_and_data_types/2.6-Famous-Quote-2.py
+++ assignments/02_variables_and_data_types/2.6-Famous-Quote-2.py
@@ -1,4 +1,4 @@
 famous_person = "Albert Einstein"
-message = f'{famous_person} once said, "A person who never made a mistake never tried anything new.""'
+message = f'{famous_person} once said, "A person who never made a mistake never tried anything new."
 
 print(message)
