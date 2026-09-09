2cc2fa44df520b5a6ed18f8d136cc446b6a91ef4
Timothy-Vaculik <timothy.vaculik001@albrigh.edu>
2026-09-09 17:37:04 -0400

    14th

--- assignments/02_variables_and_data_types/02_02_Messages.py
+++ assignments/02_variables_and_data_types/02_02_Messages.py
@@ -4,5 +4,5 @@ print(message)
 message = "This is how you can use Python to create a simple message and print it to the console."
 print(message)
 
-Takeover = "This is an errorand a big error at that, but I will fix it."
+Takeover = "This is an error and a big error at that, but I will fix it."
 print(Takeover)
