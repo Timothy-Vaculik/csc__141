1875b26fef98333c83d7594e38a734d0c809234f
Timothy-Vaculik <timothy.vaculik001@albrigh.edu>
2026-09-10 13:14:30 -0400

    100th

--- assignments/01_getting_started/01_01_python_org.py
+++ assignments/01_getting_started/01_01_python_org.py
@@ -8,4 +8,7 @@ Another thing that interest me is to be able to create a 2D game that I can play
 
 I like the fact that python is able to help me durng the school year and I can use it to figure out many problems that I have.
 
+I think I am able to find out that python is the best language ever.
+
+There is alot of projects that I am able to do with python.
 '''
