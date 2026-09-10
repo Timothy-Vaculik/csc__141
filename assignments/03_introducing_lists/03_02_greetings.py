8aaa9251e6afb1ecd0d2901d4a08aa91f80c21db
Timothy-Vaculik <timothy.vaculik001@albrigh.edu>
2026-09-10 13:47:44 -0400

    6th

---
+++ assignments/03_introducing_lists/03_02_greetings.py
@@ -0,0 +1,10 @@
+names = ["Timmy", "Aden", "Nick", "David"]
+
+messages = [
+    'Your can bench 250 pounds',
+    'Your am able to run a mile in 5 minutes',
+    'Your can do a push-up',
+    'Your can do a sit-up'
+]
+
+print(names[0] + "," + messages[0])
---
+++ assignments/03_introducing_lists/03_03_your_own_list.py
@@ -0,0 +1,7 @@
+truck = ["Ford", "Chevrolet", "Dodge", "Toyota"]
+
+
+print(f"I would like to own a {truck[0]} truck.")
+print(f"This car usually breaks down and it is {truck[1]} truck.")
+print(f"They make a very good trucks and it is called a {truck[2]} truck.")
+print(f"I like how long {truck[3]} truck lasts.")
