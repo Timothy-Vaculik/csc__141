fe179f4bda3899f2cdc824bde3a49f57551f04c1
Timothy-Vaculik <timothy.vaculik001@albrigh.edu>
2026-09-10 13:11:59 -0400

    5th

--- assignments/01_getting_started/01_01_python_org.py
+++ assignments/01_getting_started/01_01_python_org.py
@@ -1,19 +1,11 @@
 '''
 
-Look for python.org and write about stuff in the page
+The application for phython is a place where you can find many solutions to many problems. It is used for many things and it is a great language to learn.
+
+Python is great start to begin coding and there is alot of beginners that are learning how to code.
+
+Another thing that interest me is to be able to create a 2D game that I can play. 
+
+I like the fact that python is able to help me durng the school year and I can use it to figure out many problems that I have.
 
 '''
-
-'''
-
-This is going to be a good assignment to get started 
-
-'''
-
->>> print("Hello Python interpreter!")
-Hello Python interpreter!
-
-print("Hello Python world!")
-
-
-
